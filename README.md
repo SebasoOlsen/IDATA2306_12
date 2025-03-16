b7e2c1062d3a8e869172ea0550193b68b82520e4
Sebastian Olsen <Svint@hotmail.no>
2025-03-16 13:57:00 +0100

    Added files needed for spring boot

--- README.md
+++
@@ -1,2 +0,0 @@
-# IDATA2306_12
-Project for IDATA2306 group 12.
---
+++ src/test/java/IDATA2306/Group12/StayFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package IDATA2306.Group12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
