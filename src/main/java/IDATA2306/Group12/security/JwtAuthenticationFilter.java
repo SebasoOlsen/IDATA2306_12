1809268859f77a0bd22e102ff66012765a184dd9
jonasOlsenNTNU <jonaolse@stud.ntnu.no>
2025-05-20 00:09:54 +0200

    Refactor Jwt Filter to include roles.

--- src/main/java/IDATA2306/Group12/security/JwtAuthenticationFilter.java
+++ src/main/java/IDATA2306/Group12/security/JwtAuthenticationFilter.java
@@ -11,6 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -32,6 +34,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+
+        //Extract JWT token from cookie if present
         String token = null;
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
@@ -44,6 +48,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         logger.debug("Extracted token from cookie: {}", token);
 
+        if (token != null && jwtUtil.validateToken(token)) {
+            String role = jwtUtil.getRoleFromToken(token);
+            String username = jwtUtil.extractUsername(token);
+            //TODO: Decide if username or email should be used for the user
+
+            //Create authority based on role
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+
+            //Create an authentication token
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(username, null, Collections.singletonList(authority));
+
+            //Set authentication token to security context
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            //Check if the token is expiring soon and refresh if so
+            if (jwtUtil.isTokenExpiringSoon(token)) {
+                refreshToken(response, token);
+            }
+
+            logger.debug("Set authentication token to security context.");
+        }
+
+        /* OLD CODE TODO: CHECK IF NEEDED
         if (token != null) {
             if (token.split("\\.").length != 3) {
                 logger.debug("Token is invalid: expected JWT with 3 parts but got: {}", token);
@@ -64,6 +92,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         } else {
             logger.debug("No valid token provided.");
         }
+         **/
         filterChain.doFilter(request, response);
     }
 
