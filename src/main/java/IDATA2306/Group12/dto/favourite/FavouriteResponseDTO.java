e7308b66ebdfb953b4204648217db0a5a1b958e1
knut <knutolav01@gmail.com>
2025-05-23 00:43:21 +0200

    javadoc for fav dto's

--- src/main/java/IDATA2306/Group12/dto/favourite/FavouriteResponseDTO.java
+++ src/main/java/IDATA2306/Group12/dto/favourite/FavouriteResponseDTO.java
@@ -2,14 +2,20 @@ package IDATA2306.Group12.dto.favourite;
 
 import IDATA2306.Group12.dto.listing.ListingResponseDTO;
 import IDATA2306.Group12.dto.user.UserResponseDTO;
-
+/**
+ * DTO for favourite response data.
+ */
 public class FavouriteResponseDTO {
     private UserResponseDTO user;
     private ListingResponseDTO listing;
 
+    /** @return user info */
     public UserResponseDTO getUser() { return user; }
+    /** @param user user info to set */
     public void setUser(UserResponseDTO user) { this.user = user; }
 
+    /** @return listing info */
     public ListingResponseDTO getListing() { return listing; }
+    /** @param listing listing info to set */
     public void setListing(ListingResponseDTO listing) { this.listing = listing; }
 }
